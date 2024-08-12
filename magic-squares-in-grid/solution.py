4baa8c8c414f1e687ca50a03cd5a4f95e2ecbf36
Zachary Flohr <zachary.flohr@case.edu>
2024-08-12 05:35:45 -0400

    Devise shorter solution

--- magic-squares-in-grid/solution.py
+++ magic-squares-in-grid/solution.py
@@ -1,106 +1,40 @@
 """
 Author: Zachary Flohr
-Date: 2024-08-11
+Date: 2024-08-12
 """
 
 class Solution:
     """A counter of non-trivial, normal magic squares in a given grid.
 
-    Attributes:
-        Public instance methods:
-            numMagicSquaresInside: Identify and count all 3 x 3
-                non-trivial, normal, contiguous magic square subgrids
-                in a given grid of integers.
+    Public Attributes:
+        numMagicSquaresInside: Identify and count all 3 x 3 non-trivial,
+            normal, contiguous magic square subgrids in a given grid of
+            integers.
     """
 
-    def __init__(self) -> None:
-        """Initialize the instance."""
-        self._magic_order = 3
-        self._magic_square_range = [1, self._magic_order ** 2]
-        self._grid_col_index, self._grid_row_index = 0, 0
-        self._columns_satisfy_magic_square = False
-        self._rows_satisfy_magic_square = False
-        self._principal_diag_sum, self._secondary_diag_sum = 0, 0
-        self._duplicates = False
-        self._valid_num_range = True
-        self._magic_sum = None
-        self._num_magic_squares = 0
-
-    def _checkMagicSquareConstraints(self, magic_square: list[list[int]]):
+    def _checkConstraints(self, magic_square: list[list[int]],
+                          order: int) -> list[bool]:
         """Determine if magic_square is non-trivial and normal.
 
         Args:
             magic_square: A subgrid of integers to test against
                 magic square criteria.
+            order: The order of the magic square.
+
+        Returns:
+            The results of the constraint check.
         """
-        self._duplicates = False
-        self._valid_num_range = True
+        trivial = False
+        normal = True
+        normal_range = [1, order ** 2]
         flattened_magic_square = [num for row in magic_square for num in row]
         magic_square_set = set(flattened_magic_square)
         if len(flattened_magic_square) != len(magic_square_set):
-            self._duplicates = True
-        if any(num > self._magic_square_range[1]
-                or num < self._magic_square_range[0] for num
-                    in flattened_magic_square):
-            self._valid_num_range = False
-
-    def _checkMagicSquareColumnSums(self, magic_square: list[list[int]],
-                                    magic_index: int):
-        """Determine if the column sums in magic_square are equal.
-
-        Args:
-            magic_square: A subgrid of integers to test against
-                magic square criteria.
-            magic_index: The column index of magic_square.
-        """
-        col_sum = sum([row[magic_index] for row in magic_square])
-        if not self._magic_sum:
-            self._magic_sum = col_sum
-        if self._magic_sum != col_sum:
-            self._magic_sum = None
-            self._grid_col_index += magic_index
-        if magic_index == self._magic_order - 1:
-            self._columns_satisfy_magic_square = True
-
-    def _checkMagicSquareRowSums(self, magic_square: list[list[int]],
-                                 magic_index: int):
-        """Determine if the row sums in magic_square are equal.
-
-        Args:
-            magic_square: A subgrid of integers to test against
-                magic square criteria.
-            magic_index: The row index of magic_square.
-        """
-        row_sum = sum(magic_square[magic_index])
-        if self._magic_sum != row_sum:
-            self._magic_sum = None
-            self._grid_col_index += 1
-            self._columns_satisfy_magic_square = False
-        if magic_index == self._magic_order - 1:
-            self._rows_satisfy_magic_square = True
-
-    def _checkMagicSquareDiagonalSums(self, magic_square: list[list[int]],
-                                      magic_index: int):
-        """Determine if the diagonal sums in magic_square are equal.
-
-        Args:
-            magic_square: A subgrid of integers to test against
-                magic square criteria.
-            magic_index: An index used to obtain the diagonals of
-                magic_square.
-        """
-        self._principal_diag_sum += magic_square[magic_index][magic_index]
-        self._secondary_diag_sum += magic_square[
-                self._magic_order - 1 - magic_index][magic_index]
-        if magic_index == self._magic_order - 1:
-            self._grid_col_index += 1
-            self._columns_satisfy_magic_square = False
-            self._rows_satisfy_magic_square = False
-            if (self._magic_sum == self._principal_diag_sum
-                    and self._magic_sum == self._secondary_diag_sum):
-                self._num_magic_squares += 1
-            self._principal_diag_sum, self._secondary_diag_sum = 0, 0
-            self._magic_sum = None
+            trivial = True
+        if (any(num > normal_range[1] or num < normal_range[0]
+                for num in flattened_magic_square)):
+            normal = False
+        return [trivial, normal]
 
     def numMagicSquaresInside(self, grid: list[list[int]]) -> int:
         """Find and count all non-trivial, normal magic squares in grid.
@@ -109,56 +43,39 @@ class Solution:
             grid: A grid of integers to examine for magic squares.
 
         Returns:
-            The number of non-trivial, normal magic squares of order
-                self._magic_order in grid.
+            The number of non-trivial, normal magic squares of order 3.
         """
-        self.__init__()
-        total_grid_rows = len(grid)
-        total_grid_columns = len(grid[0])
-        if (total_grid_rows >= self._magic_order
-                and total_grid_columns >= self._magic_order):
-            while (self._grid_row_index
-                    + self._magic_order - 1 < total_grid_rows):
-                while (self._grid_col_index
-                        + self._magic_order - 1 < total_grid_columns):
-                    for magic_index in range(self._magic_order):
-                        tested_magic_square = [
-                            row[self._grid_col_index : self._grid_col_index
-                                    + self._magic_order]
-                                for row in grid[
-                                    self._grid_row_index : self._grid_row_index
-                                        + self._magic_order]]
-                        self._checkMagicSquareConstraints(tested_magic_square)
-                        if self._duplicates or not self._valid_num_range:
-                            self._grid_col_index += 1
-                            self._duplicates = False
-                            self._valid_num_range = True
-                            break
-                        if not self._columns_satisfy_magic_square:
-                            self._checkMagicSquareColumnSums(
-                                tested_magic_square,
-                                magic_index)
-                            if (not self._magic_sum
-                                    or self._columns_satisfy_magic_square):
-                                break
-                        if (self._columns_satisfy_magic_square
-                                and not self._rows_satisfy_magic_square):
-                            self._checkMagicSquareRowSums(
-                                tested_magic_square,
-                                magic_index)
-                            if (not self._magic_sum
-                                    and not self._columns_satisfy_magic_square
-                                    or self._rows_satisfy_magic_square):
-                                break
-                        if (self._columns_satisfy_magic_square
-                                and self._rows_satisfy_magic_square):
-                            self._checkMagicSquareDiagonalSums(
-                                tested_magic_square,
-                                magic_index)
-                            if (not self._magic_sum
-                                    and not self._columns_satisfy_magic_square
-                                    and not self._rows_satisfy_magic_square):
-                                break
-                self._grid_col_index = 0
-                self._grid_row_index += 1
-        return self._num_magic_squares
+        order = 3 
+        magic_squares = 0
+        for i in range(len(grid)):
+            if i + order > len(grid):
+                break
+            for j in range(len(grid[i])):
+                if j + order > len(grid[i]):
+                    break
+                principal_diag_sum, secondary_diag_sum = 0, 0
+                magic_sum = None
+                trivial = False
+                normal = True
+                for k in range(order):
+                    magic_square = [
+                        row[j : j + order] for row in grid[i : i + order]]
+                    trivial, normal = (
+                        self._checkConstraints(magic_square, order))
+                    if trivial or not normal:
+                        break
+                    row_sum = sum(magic_square[k])
+                    col_sum = sum([row[k] for row in magic_square])
+                    principal_diag_sum += magic_square[k][k]
+                    secondary_diag_sum += magic_square[order - 1 - k][k]
+                    if not magic_sum:
+                        magic_sum = row_sum
+                    if (row_sum != col_sum or magic_sum != row_sum
+                            or principal_diag_sum > row_sum
+                            or secondary_diag_sum > row_sum):
+                        break
+                    if k == order - 1:
+                        if (principal_diag_sum == secondary_diag_sum 
+                                and principal_diag_sum == magic_sum):
+                            magic_squares += 1
+        return magic_squares
